e6b90e645d9a113c0393afc84f51434c7bc8152e
Shashank0271 <ssingh0271@gmail.com>
2022-04-06 19:10:56 +0530

    updated files

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
+import 'dart:typed_data';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -15,6 +16,9 @@ const apiKey = 'AIzaSyBhCqHm1BrofyrPtltsmJ2eu5B7lAkMURs';
 class _LocationInputState extends State<LocationInput> {
   String? imageUrl;
   CameraPosition? _cameraPosition;
+  GoogleMapController? _mapController;
+  Uint8List? _imageBytes;
+  Completer<GoogleMapController> _controller = Completer();
 
   Future<void> _getCurrentLocation() async {
     var locationData = await Location().getLocation();
@@ -24,30 +28,55 @@ class _LocationInputState extends State<LocationInput> {
       target: LatLng(latitude, longitude),
       zoom: 14.4746,
     );
-    setState(() {});
+
+    // GoogleMap(
+    //   initialCameraPosition: _cameraPosition!,
+    //   onMapCreated: (GoogleMapController controller) {
+    //     _controller.complete(controller);
+    //     _mapController = controller;
+    //   },
+    // );
+    //newb comment
+
+    Uint8List? imageBytes = await _mapController?.takeSnapshot();
+    setState(() {
+      _imageBytes = imageBytes;
+    });
+    // setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    Completer<GoogleMapController> _controller = Completer();
     return Column(
       children: [
         Container(
-          height: 200,
+          height: 0,
           width: double.infinity,
           child: _cameraPosition == null
               ? const Center(child: Text('No location Chosen'))
               : GoogleMap(
                   initialCameraPosition: _cameraPosition!,
-                  onMapCreated: (GoogleMapController controller) {
+                  onMapCreated: (GoogleMapController controller) async {
                     _controller.complete(controller);
+                    _mapController = controller;
+                    Uint8List? imageBytes = await controller.takeSnapshot();
+                    setState(() {
+                      _imageBytes = imageBytes;
+                    });
                   },
                 ),
           decoration: BoxDecoration(
             border: Border.all(color: Colors.grey),
           ),
         ),
-        const SizedBox(height: 20),
+        SizedBox(
+          height: 200,
+          child: _imageBytes != null
+              ? Image.memory(_imageBytes!)
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
